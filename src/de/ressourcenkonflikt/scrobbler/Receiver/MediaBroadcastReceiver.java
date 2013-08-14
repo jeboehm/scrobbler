69f68833554b5786e7acde2f38e33a94807109ca
Jeffrey Boehm <post@jeffrey-boehm.de>
2013-08-14 23:44:31 +0200

    Send event when new song is in the queue

--- src/de/ressourcenkonflikt/scrobbler/Receiver/MediaBroadcastReceiver.java
+++ src/de/ressourcenkonflikt/scrobbler/Receiver/MediaBroadcastReceiver.java
@@ -22,22 +22,31 @@ public class MediaBroadcastReceiver extends BroadcastReceiver {
 
         if (intent.getAction().contentEquals("com.android.music.metachanged")) {
             if (artist != null && track != null) {
-                queueSong(artist, track);
+                if (queueSong(artist, track)) {
+                    // Send new event
+                    Intent new_event = new Intent();
+                    new_event.setAction("de.ressourcenkonflikt.scrobbler.queue.newitem");
+                    context.sendBroadcast(new_event);
+                }
             }
         }
     }
 
-    private void queueSong(String artist, String track) {
+    private boolean queueSong(String artist, String track) {
         Queue queue = Queue.getInstance();
         Song song = new Song(artist, track);
 
         if (queue.add(song)) {
             Log.i(getClass().getCanonicalName(),
                     String.format("Added '%1$s - %2$s' to the queue.", artist, track));
+
+            return true;
         } else {
             Log.i(getClass().getCanonicalName(),
                     String.format("Could not add '%1$s - %2$s' to the queue, recently played.",
                             artist, track));
         }
+
+        return false;
     }
 }
