c9be803e5090592524736472abeb0f549b29aec3
Jeffrey Boehm <post@jeffrey-boehm.de>
2013-08-18 22:21:06 +0200

    Moved some code

--- src/de/ressourcenkonflikt/scrobbler/Receiver/MediaBroadcastReceiver.java
+++ src/de/ressourcenkonflikt/scrobbler/Receiver/MediaBroadcastReceiver.java
@@ -3,9 +3,6 @@ package de.ressourcenkonflikt.scrobbler.Receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import de.ressourcenkonflikt.scrobbler.SongQueue.Queue;
-import de.ressourcenkonflikt.scrobbler.SongQueue.Song;
 
 /**
  * Created with IntelliJ IDEA.
@@ -22,31 +19,17 @@ public class MediaBroadcastReceiver extends BroadcastReceiver {
 
         if (intent.getAction().contentEquals("com.android.music.metachanged")) {
             if (artist != null && track != null) {
-                if (queueSong(artist, track)) {
-                    // Send new event
-                    Intent new_event = new Intent();
-                    new_event.setAction("de.ressourcenkonflikt.scrobbler.queue.newitem");
-                    context.sendBroadcast(new_event);
-                }
+                // Send new event
+                Intent new_event = new Intent();
+
+                new_event.setAction("de.ressourcenkonflikt.scrobbler.queue.newitem");
+
+                new_event.putExtra("song_artist", artist);
+                new_event.putExtra("song_album", album);
+                new_event.putExtra("song_track", track);
+
+                context.sendBroadcast(new_event);
             }
         }
-    }
-
-    private boolean queueSong(String artist, String track) {
-        Queue queue = Queue.getInstance();
-        Song song = new Song(artist, track);
-
-        if (queue.add(song)) {
-            Log.i(getClass().getCanonicalName(),
-                    String.format("Added '%1$s - %2$s' to the queue.", artist, track));
-
-            return true;
-        } else {
-            Log.i(getClass().getCanonicalName(),
-                    String.format("Could not add '%1$s - %2$s' to the queue, recently played.",
-                            artist, track));
-        }
-
-        return false;
     }
 }
--- src/de/ressourcenkonflikt/scrobbler/Receiver/QueueNewSongReceiver.java
+++ src/de/ressourcenkonflikt/scrobbler/Receiver/QueueNewSongReceiver.java
@@ -3,7 +3,9 @@ package de.ressourcenkonflikt.scrobbler.Receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import de.ressourcenkonflikt.scrobbler.SongQueue.Queue;
+import de.ressourcenkonflikt.scrobbler.SongQueue.Song;
 import de.ressourcenkonflikt.scrobbler.Util.ConnectivityChecker;
 import de.ressourcenkonflikt.scrobbler.Util.ScrobbleHandler;
 
@@ -18,16 +20,36 @@ public class QueueNewSongReceiver extends BroadcastReceiver {
     ScrobbleHandler handler;
 
     public void onReceive(Context context, Intent intent) {
-        if (handler == null) {
-            handler = new ScrobbleHandler(new ConnectivityChecker(context), context);
-        }
+        if (queueSong(intent.getStringExtra("song_artist"), intent.getStringExtra("song_track"))) {
+            if (handler == null) {
+                handler = new ScrobbleHandler(new ConnectivityChecker(context), context);
+            }
 
-        while (Queue.getInstance().getSize() > 0) {
-            int result = handler.scrobbleSong(Queue.getInstance().get());
+            while (Queue.getInstance().getSize() > 0) {
+                int result = handler.scrobbleSong(Queue.getInstance().get());
 
-            if (result == ScrobbleHandler.RESULT_STOP) {
-                break;
+                if (result == ScrobbleHandler.RESULT_STOP) {
+                    break;
+                }
             }
         }
+    }
+
+    private boolean queueSong(String artist, String track) {
+        Queue queue = Queue.getInstance();
+        Song song = new Song(artist, track);
+
+        if (queue.add(song)) {
+            Log.i(getClass().getCanonicalName(),
+                    String.format("Added '%1$s - %2$s' to the queue.", artist, track));
+
+            return true;
+        } else {
+            Log.i(getClass().getCanonicalName(),
+                    String.format("Could not add '%1$s - %2$s' to the queue, recently played.",
+                            artist, track));
+        }
+
+        return false;
     }
 }
