05a5208e56889ffe4760ddad268b8ff40b1ac379
Jeffrey Boehm <post@jeffrey-boehm.de>
2013-08-28 00:06:15 +0200

    License update

--- src/de/ressourcenkonflikt/scrobbler/LastFm/Client.java
+++ src/de/ressourcenkonflikt/scrobbler/LastFm/Client.java
@@ -13,11 +13,24 @@ import java.net.UnknownHostException;
 import java.util.Date;
 
 /**
- * Created with IntelliJ IDEA.
- * User: jeff
- * Date: 11.08.13
- * Time: 23:53
- * To change this template use File | Settings | File Templates.
+ * This file is part of scrobbler for ASTEROID.
+ *
+ * https://github.com/jeboehm/scrobbler
+ *
+ * Copyright (C) 2013 Jeffrey Boehm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class Client {
     private static Client ourInstance = new Client();
